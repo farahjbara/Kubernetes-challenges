7678db663a61845b47e41bc2b21a479f79ff9b30
farah jbara <odev7050@gmail.com>
2023-12-21 12:13:40 +0100

    Deployment object

--- week1/CoreConcepts.sh
+++ week1/CoreConcepts.sh
@@ -10,6 +10,7 @@ kubectl describe node <node-name> #To see detailed information about a node, inc
 #Events are objects that provide insight into what is happening inside a cluster, 
 kubectl get events #Used in the context of Kubernetes to list events
   
+
 #kubelet : An agent that runs on each node in the cluster. It makes sure that containers are running in a pod.
 
 systemctl status kubelet.
